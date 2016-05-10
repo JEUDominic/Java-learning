85a59daa4502f5ec203bd8febfc9b2731603f50b
username <xiaoqinaide@live.cn>
2016-05-10 15:42:44 +0800

    Add School

--- Alumni.java
+++ Alumni.java
@@ -1,5 +1,24 @@
 package Table;
 
 public class Alumni {
-	private
+	// Attributes for Alumni...
+	private String name;
+	private char[] studentID;
+	private boolean sex; // True for man, false for woman...
+	private String major;
+	private int graYear;
+	private int state;
+	private char[] id;
+	
+	Alumni(String name, char[] studentID, boolean sex, String major, int graYear,int state, char[] id){
+		this.name = name;
+		this.studentID = studentID;
+		this.sex = sex;
+		this.major = major;
+		this.graYear = graYear;
+		this.state = state;
+		this.id = id;
+	}
+	
+	
 }
---
+++ School.java
@@ -0,0 +1,16 @@
+package Table;
+
+public class School {
+	// Attributes...
+	private String name;
+	private String country;
+	private String city;
+	private boolean top100;
+	
+	// Constructor...
+	School(String name, String country , String city, boolean top100){
+		this.name = name;
+		this.country = country;
+		
+	}
+}

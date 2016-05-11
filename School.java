c10e3cb3574f6af7f432a1a09936c1b6242faaef
username <xiaoqinaide@live.cn>
2016-05-11 23:44:21 +0800

    Modified school

--- School.java
+++ School.java
@@ -1,6 +1,6 @@
 package Table;
 
-public class School {
+public class School extends Organization{
 	// Attributes...
 	private String name;
 	private String country;
@@ -9,8 +9,11 @@ public class School {
 	
 	// Constructor...
 	School(String name, String country , String city, boolean top100){
-		this.name = name;
-		this.country = country;
-		
+		super(name,country,city);
+		this.top100 = top100;	
 	}
+	
+	public void setTop100(boolean top100){ this.top100 = top100;}
+	
+	public boolean getTop100(){ return top100;}
 }
